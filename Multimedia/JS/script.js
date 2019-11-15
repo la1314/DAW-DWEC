ff16b0cf034a27c2f207fe8cfb70d2cc5dcad84a
la1314 <tsue00@gmail.com>
2019-11-15 18:26:57 +0100

    Añadidos estilos

---
+++ Multimedia/JS/script.js
@@ -0,0 +1,51 @@
+function init(){
+
+    //TODO
+
+}
+
+function silence(){
+    //TODO
+}
+
+function playPause(){
+    //TODO
+}
+
+function retroceder(){
+    //TODO
+}
+
+function adelantar(){
+    //TODO
+}
+
+function reiniciar(){
+    //TODO
+}
+
+function bajarVol(){
+    //TODO
+}
+
+function subirVol(){
+    //TODO
+}
+
+function reproducir(){
+    //TODO
+}
+
+function lanzarPublicidad(){
+    //TODO
+}
+
+function deshabilitarBotones(){
+    //TODO
+}
+
+function barraProgresion(){
+    //TODO
+}
+
+window.onload = init;
