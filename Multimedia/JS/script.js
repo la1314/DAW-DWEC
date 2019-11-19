02c533cdc95c5c545fdd57ee6568cc0a8774ac20
la1314 <tsue00@gmail.com>
2019-11-19 20:31:04 +0100

    Añadiendo cambios

--- Multimedia/JS/script.js
+++ Multimedia/JS/script.js
@@ -1,51 +1,82 @@
-function init(){
+function init() {
 
     //TODO
 
+    agregarLista(6);
+
 }
 
-function silence(){
+function agregarLista(nVideos) {
+
+
+    let lista = document.getElementById('lista');
+
+
+    for (let index = 0; index < nVideos; index++) {
+
+        let video = document.createElement('div');
+        video.style.backgroundImage = 'url("'+ArrayVideos[index]+'")';
+        video.classList.add('videos');
+        lista.appendChild(video);
+
+    }
+
+}
+
+
+function silence() {
     //TODO
 }
 
-function playPause(){
+function playPause() {
     //TODO
 }
 
-function retroceder(){
+function retroceder() {
     //TODO
 }
 
-function adelantar(){
+function adelantar() {
     //TODO
 }
 
-function reiniciar(){
+function reiniciar() {
     //TODO
 }
 
-function bajarVol(){
+function bajarVol() {
     //TODO
 }
 
-function subirVol(){
+function subirVol() {
     //TODO
 }
 
-function reproducir(){
+function reproducir() {
     //TODO
 }
 
-function lanzarPublicidad(){
+function lanzarPublicidad() {
     //TODO
 }
 
-function deshabilitarBotones(){
+function deshabilitarBotones() {
     //TODO
 }
 
-function barraProgresion(){
+function barraProgresion() {
     //TODO
 }
 
 window.onload = init;
+
+
+let ArrayVideos = new Array();
+
+ArrayVideos.push('http://i.ytimg.com/vi/hLTgQ5SC-PU/sddefault.jpg');
+ArrayVideos.push('http://i.ytimg.com/vi/hLTgQ5SC-PU/sddefault.jpg');
+ArrayVideos.push('http://i.ytimg.com/vi/hLTgQ5SC-PU/sddefault.jpg');
+ArrayVideos.push('http://i.ytimg.com/vi/hLTgQ5SC-PU/sddefault.jpg');
+ArrayVideos.push('http://i.ytimg.com/vi/hLTgQ5SC-PU/sddefault.jpg');
+ArrayVideos.push('http://i.ytimg.com/vi/hLTgQ5SC-PU/sddefault.jpg');
+
