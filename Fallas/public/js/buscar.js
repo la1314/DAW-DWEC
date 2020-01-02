d656f596a9258bdaa6266bdd9bcb698b1879372d
la1314 <44264388+la1314@users.noreply.github.com>
2020-01-02 18:38:25 +0100

    Añadidas opciones de filtro por año y listado por demografia

--- Fallas/public/js/buscar.js
+++ Fallas/public/js/buscar.js
@@ -6,18 +6,17 @@
 
 
 // Algunos valores
-
-const recargaElectricaURL = "http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON";
+const fallasValenciaURL = "http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON";
 let fallasValencia;
-let tipo;
+let estadoActual;
 
 // Esta es la funcion de filtrado para
 // obtener tan solo los elementos que cumplen
 // una serie de requisitos.
 
 function filtroLetra(elemento) {
-    let letra = document.querySelector(`input[name="calle"]`).value;
-    return elemento.properties.calle.startsWith(letra);
+  let letra = document.querySelector(`input[name="calle"]`).value;
+  return elemento.properties.calle.startsWith(letra);
 }
 
 
@@ -25,146 +24,142 @@ function filtroLetra(elemento) {
 // se lanza cada vez que se realiza una insercion en
 // el texto del nombre.
 function toUpp() {
-    document.querySelector(`input[name="calle"]`).value = document.querySelector(`input[name="calle"]`).value.toUpperCase();
+  document.querySelector(`input[name="calle"]`).value = document.querySelector(`input[name="calle"]`).value.toUpperCase();
 }
 
+//funcion que se lanza para listar todas las fallas sin ningún tipo de filtro aplicado tomando en cuenta la demografia seleccionada
+function porDefecto(datos) {
+  buscar(datos, returnDemografia());
+}
 
-function buscar() {
+//funcioón que se encarga de dibujar los cuardros de las fallas a este se le pasa como parámetros los datos a dibujar y la demografia de a seleccionar para mostrarlos por pantalla
+// cada vez que se llama a esta función guarda en la variable estadoActual los datos de datosFalla puesto que las diferentes opciones toman como base estos datos para ir filtrandolos
 
-    //  console.log(fallasValencia);
+function buscar(datosFalla, demografia) {
 
-    // Obtenemos el JSON que esta definido   
+  // console.log(tipo);
+  // console.log(resultado[0]);
 
-    tipo = this.value;
+  estadoActual = datosFalla;
 
-    // console.log(tipo);
+  let myNode = document.querySelector(".resultados");
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+  }
 
-    // console.log(resultado[0]);
+  //Dibujamos el cuadro de fallas
+  datosFalla.forEach(falla => {
 
-    let myNode = document.querySelector(".resultados");
-    while (myNode.firstChild) {
-        myNode.removeChild(myNode.firstChild);
+    // Creamos el cuadro de cada falla
+    let cuadro = document.createElement('div');
+    let boceto = document.createElement('div');
+    let divNombre = document.createElement('div');
+    let nombre = document.createElement('p');
+    let ubicacion = document.createElement('button');
+
+    cuadro.classList.add('cuadrado');
+    boceto.classList.add('imagenes');
+    divNombre.classList.add('nombres');
+    ubicacion.classList.add('botones');
+
+    if (demografia == 'children') {
+      boceto.style.backgroundImage = 'url("' + falla.properties.boceto_i + '")';
+    } else if (demografia == 'adult') {
+      boceto.style.backgroundImage = 'url("' + falla.properties.boceto + '")';
     }
 
-    // Una vez tenemos el listado filtrado pasamos a crear
-    // cada uno de los <li> que representan
+    nombre.innerHTML = falla.properties.nombre;
+    ubicacion.innerHTML = "Ubicación";
 
+    divNombre.appendChild(nombre);
+    cuadro.appendChild(boceto);
+    cuadro.appendChild(divNombre);
+    cuadro.appendChild(ubicacion);
 
-    // Por cada uno de ellos
-
-    fallasValencia.forEach(falla => {
-
-        // Creamos el cuadro de cada falla
-        let cuadro = document.createElement('div');
-        let boceto = document.createElement('div');
-        let divNombre = document.createElement('div');
-        let nombre = document.createElement('p');
-        let ubicacion = document.createElement('button');
-
-        cuadro.classList.add('cuadrado');
-        boceto.classList.add('imagenes');
-        divNombre.classList.add('nombres');
-        ubicacion.classList.add('botones');
-
-        if (tipo == 'children') {
-            boceto.style.backgroundImage = 'url("' + falla.properties.boceto_i + '")';
-
-        } else {
-            boceto.style.backgroundImage = 'url("' + falla.properties.boceto + '")';
-        }
-
-        nombre.innerHTML = falla.properties.nombre;
-        ubicacion.innerHTML = "Ubicación";
-
-        divNombre.appendChild(nombre);
-        cuadro.appendChild(boceto);
-        cuadro.appendChild(divNombre);
-        cuadro.appendChild(ubicacion);
-
-        document.querySelector(".resultados").appendChild(cuadro);
-    });
-
-
+    document.querySelector(".resultados").appendChild(cuadro);
+  });
 
 }
 
-//TODO
+//funcion que filtra estadoActual dependiendo a la demografia para obtener las fallas que han sido creadas desde el valor actual de la opción
 function filtroDesde() {
 
-    console.log(this.value);
+  let demografia = returnDemografia();
+  let respaldo;
 
-    //let respaldo = fallasValencia.features.filter(falla.properties.anyo_fundacion > this.value);
-    let respaldo = fallasValencia.filter( año => año.properties.anyo_fundacion > this.value  );
+  if (demografia == 'adult') {
+    respaldo = estadoActual.filter(año => año.properties.anyo_fundacion > this.value);
+  } else {
+    respaldo = estadoActual.filter(año => año.properties.anyo_fundacion_i > this.value);
+  }
 
-    console.log(respaldo);
-    
-        let myNode = document.querySelector(".resultados");
-        while (myNode.firstChild) {
-            myNode.removeChild(myNode.firstChild);
-        }
+  buscar(respaldo, demografia);
 
-        respaldo.forEach(falla => {
+}
 
-            // Creamos el cuadro de cada falla
-            let cuadro = document.createElement('div');
-            let boceto = document.createElement('div');
-            let divNombre = document.createElement('div');
-            let nombre = document.createElement('p');
-            let ubicacion = document.createElement('button');
+//funcion que filtra estadoActual dependiendo a la demografia para obtener las fallas que han sido creadas hsata el valor actual de la opción
+function filtroHasta() {
 
-            cuadro.classList.add('cuadrado');
-            boceto.classList.add('imagenes');
-            divNombre.classList.add('nombres');
-            ubicacion.classList.add('botones');
+  let demografia = returnDemografia();
+  let respaldo;
 
-            if (tipo == 'children') {
-                boceto.style.backgroundImage = 'url("' + falla.properties.boceto_i + '")';
+  if (demografia == 'adult') {
+    respaldo = estadoActual.filter(año => año.properties.anyo_fundacion < this.value);
+  } else {
+    respaldo = estadoActual.filter(año => año.properties.anyo_fundacion_i < this.value);
+  }
 
-            } else {
-                boceto.style.backgroundImage = 'url("' + falla.properties.boceto + '")';
-            }
+  buscar(respaldo, demografia);
+}
 
-            nombre.innerHTML = falla.properties.nombre;
-            ubicacion.innerHTML = "Ubicación";
+//Devuelve el tipo de demografia seleccionado actualmente en las opciones
+function returnDemografia() {
 
-            divNombre.appendChild(nombre);
-            cuadro.appendChild(boceto);
-            cuadro.appendChild(divNombre);
-            cuadro.appendChild(ubicacion);
+  let radius = document.querySelectorAll('input[name="tipoFalla"]');
+  let demografia;
+  for (var i = 0; i < radius.length; i++) {
+    if (radius[i].checked == true) {
+      demografia = radius[i].value;
+    }
+  }
+  return demografia;
 
-            document.querySelector(".resultados").appendChild(cuadro);
-        });
+}
 
+//Lista los datos actuales dependiendo de de la demografia seleccionada
+function filtroDemografia() {
+
+  buscar(estadoActual, this.value);
 
 }
 
 function init() {
 
-    // Binding de los eventos correspondientes.
+  // Binding de los eventos correspondientes.
 
-    // Click en el boton de buscar
-    // document.querySelector(`input[type="button"]`).addEventListener("click", buscar);
-    // Texto cambia en el <input>
-    // document.querySelector(`input[type="text"]`).addEventListener("input", toUpp);
+  // Click en el boton de buscar
+  // document.querySelector(`input[type="button"]`).addEventListener("click", buscar);
+  // Texto cambia en el <input>
+  // document.querySelector(`input[type="text"]`).addEventListener("input", toUpp);
 
-    const fetchPromesa = fetch(recargaElectricaURL);
-    // Cuando se resuelva la promesa
-    fetchPromesa.then(response => {
-        // la pasamos a JSON
-        return response.json();
+  const fetchPromesa = fetch(fallasValenciaURL);
+  // Cuando se resuelva la promesa
+  fetchPromesa.then(response => {
+    // la pasamos a JSON
+    return response.json();
 
-        // Y entonces
-    }).then(respuesta => {
+    // Y entonces
+  }).then(respuesta => {
 
-        fallasValencia = respuesta.features
-        buscar();
+    fallasValencia = respuesta.features
+    porDefecto(fallasValencia);
 
-    });
+  });
 
 
-    document.querySelectorAll('input[name="tipoFalla"]').forEach(radius => radius.addEventListener('change', buscar));
-    document.querySelector('input[name="fechaDesde"]').addEventListener('change', filtroDesde);
-
+  document.querySelectorAll('input[name="tipoFalla"]').forEach(radius => radius.addEventListener('change', filtroDemografia));
+  document.querySelector('input[name="fechaDesde"]').addEventListener('change', filtroDesde);
+  document.querySelector('input[name="fechaHasta"]').addEventListener('change', filtroHasta);
 
 }
 
